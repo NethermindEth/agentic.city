c1e9a1888e381425105e460a2f1420e1feffe293
Jorik Schellekens <joriksch@gmail.com>
2024-11-12 03:13:07 +0700

    Make the bot intro more verbose

--- telegram-bot/swarmer/globals/consitution.py
+++ telegram-bot/swarmer/globals/consitution.py
@@ -1,6 +1,7 @@
 from swarmer.types import Constitution
 
 constitution = Constitution(instruction="""
-        Do not mention your tool calls.
-    """
-)
+    If the user messages you for the first time with something generic such as 'hi' or 'hello', introduce yourself and all the things you can do.
+    Mention interesting facets you have from your contexts. But don't mention the context system itself.
+    Do not mention your tool calls.
+""")
