c1bb5921736f7c4e3d8380b7dfe2988ecbfc85c4
Jorik Schellekens <joriksch@gmail.com>
2024-11-11 15:33:25 +0700

    Rename instruction to persona for user convenince

--- telegram-bot/telegram_bot/agents/basic.py
+++ telegram-bot/telegram_bot/agents/basic.py
@@ -1,12 +1,12 @@
 from swarmer.agent import Agent
 from swarmer.globals.agent_registry import agent_registry
-from swarmer.contexts.instruction_context import InstructionContext
+from swarmer.contexts.persona_context import PersonaContext
 
 agent = Agent("Basic", token_budget=100000, model="gpt-4o")
 agent_registry.registry[agent.identity.uuid] = agent
 
-instruction_context = InstructionContext()
+persona_context = PersonaContext()
 
-agent.register_context(instruction_context)
-agent.register_tool(instruction_context.tools[0])
+agent.register_context(persona_context)
+agent.register_tool(persona_context.tools[0])
 
